c4f252c89321acd3ad3f3d79b125368f0bd6cce2
szatms <161427732+szatms@users.noreply.github.com>
2025-05-10 22:05:41 +0200

    Update connection.php
    Took out unnecessary comments.

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "127.0.0.1"; //"fdb1027.runhosting.com";
-    $username = "root"; //"4618425_adatok";
-    $password = ""; //"SG{mdkfh2T,-+dMK";
-    $db_name = "adatok"; //"4618425_adatok";
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $db_name = "adatok";
     $portNum = 3306;
 
     $con = new mysqli($servername, $username, $password, $db_name, $portNum);

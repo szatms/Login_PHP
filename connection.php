dc073a90d0043e74022759babca1fae0b96edb14
szatms <161427732+szatms@users.noreply.github.com>
2025-05-10 22:04:58 +0200

    Made a revision in connection.php
    New default is now for a local database, hosted by the user.

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "fdb1027.runhosting.com";
-    $username = "4618425_adatok";
-    $password = "SG{mdkfh2T,-+dMK";
-    $db_name = "4618425_adatok";
+    $servername = "127.0.0.1"; //"fdb1027.runhosting.com";
+    $username = "root"; //"4618425_adatok";
+    $password = ""; //"SG{mdkfh2T,-+dMK";
+    $db_name = "adatok"; //"4618425_adatok";
     $portNum = 3306;
 
     $con = new mysqli($servername, $username, $password, $db_name, $portNum);
